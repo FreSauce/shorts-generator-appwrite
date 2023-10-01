e6a28949cae2f130ec22faae72b36f9bd191d6dc
Shardul Kurdukar <24shardul@gmail.com>
2023-10-01 20:59:19 +0530

    getting info

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
+import axios from "axios"
 import fs from "fs"
 import youtubedl from "youtube-dl-exec"
 import ytdl from "ytdl-core"
 
 import { getStaticFile, throwIfMissing } from "./utils.js"
-
-// ytdl("https://www.youtube.com/watch?v=AUCRA3k7w2Q")
-//   .pipe(fs.createWriteStream("video.mp4"))
-//   .addListener("finish", () => {
-//     console.log("finished")
-//   })
-
-// let out = await youtubedl("https://www.youtube.com/watch?v=6xKWiCMKKJg", {
-//   dumpSingleJson: true,
-//   noCheckCertificates: true,
-//   noWarnings: true,
-//   preferFreeFormats: true,
-//   addHeader: ["referer:youtube.com", "user-agent:googlebot"]
-// })
-// console.log(out)
 
 export default async (context) => {
   const { req, res } = context
@@ -30,47 +16,46 @@ export default async (context) => {
   }
 
   // try {
-  //   throwIfMissing(req.body, ["videourl"])
+  //   throwIfMissing(req.body, ["videoid"])
   // } catch (err) {
   //   return res.json({ ok: false, error: err.message }, 400)
   // }
 
   try {
-    context.log(req.body.videourl)
-    // let info = await ytdl.getBasicInfo(req.body.videourl)
+    context.log(req.body.videoid)
+    // let info = await ytdl.getBasicInfo(req.body.videoid)
 
-    let out = await youtubedl("https://www.youtube.com/watch?v=6xKWiCMKKJg", {
-      dumpSingleJson: true,
-      noCheckCertificates: true,
-      noWarnings: true,
-      preferFreeFormats: true,
-      addHeader: ["referer:youtube.com", "user-agent:googlebot"]
-    })
-    // let info = "hehe"
-    // context.log(info)
-    context.log(out)
-    let wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-    // await wait(10000)
+    const options = {
+      method: "GET",
+      url: "https://get-youtube-video-revision-graph.p.rapidapi.com/getHeatMarkers",
+      params: {
+        videoId: videoid
+      },
+      headers: {
+        "X-RapidAPI-Key":
+          process.env.RAPIDAPI_KEY ||
+          "29d59d18b1msh9cc3ad5deb22decp1992c6jsnee8301ad5d8c",
+        "X-RapidAPI-Host": "get-youtube-video-revision-graph.p.rapidapi.com"
+      }
+    }
+
+    try {
+      const response = await axios.request(options)
+      context.log(response.data)
+    } catch (error) {
+      context.error(error)
+    }
+
+    // let out = await youtubedl("https://www.youtube.com/watch?v=6xKWiCMKKJg", {
+    //   dumpSingleJson: true,
+    //   noCheckCertificates: true,
+    //   noWarnings: true,
+    //   preferFreeFormats: true,
+    //   addHeader: ["referer:youtube.com", "user-agent:googlebot"]
+    // })
     return res.json({ ok: false, error: "Failed to download video" }, 200)
   } catch (err) {
     context.log(err)
     return res.json({ ok: false, error: err.message }, 400)
   }
-  // const openai = new OpenAIApi(
-  //   new Configuration({
-  //     apiKey: process.env.OPENAI_API_KEY
-  //   })
-  // )
-
-  // try {
-  //   const response = await openai.createChatCompletion({
-  //     model: "gpt-3.5-turbo",
-  //     max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS ?? "512"),
-  //     messages: [{ role: "user", content: req.body.prompt }]
-  //   })
-  //   const completion = response.data.choices[0].message?.content
-  //   return res.json({ ok: true, completion }, 200)
-  // } catch (err) {
-  //   return res.json({ ok: false, error: "Failed to query model." }, 500)
-  // }
 }
